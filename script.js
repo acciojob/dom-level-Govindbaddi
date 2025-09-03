e724d5697e6f7a65f6ced0c9259799644689cd3c
Govindbaddi <baddigovindarao710@gmail.com>
2025-09-03 15:44:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const presentlevel=document.getElementById("level")
+var count=1
+if(presentlevel.parent==null){
+	//alert(`The level of the element is: ${count}`)
+}
+else{
+	presentlevel=presentlevel.parent
+	count++
+}
+}
