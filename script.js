156c56c8440a3c0833006969739794272cc0932e
Govindbaddi <baddigovindarao710@gmail.com>
2025-09-03 15:47:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,9 @@
 const presentlevel=document.getElementById("level")
 var count=1
 if(presentlevel.parent==null){
-	//alert(`The level of the element is: ${count}`)
+	alert(`The level of the element is: ${count}`)
 }
 else{
 	presentlevel=presentlevel.parent
 	count++
-}
 }
